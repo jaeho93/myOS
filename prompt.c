89b66547400f03f70634ef3eab679cf9417383dc
a4382432 <rlacksals96@gmail.com>
2016-12-05 17:27:37 +0900

    prompt(명령어 판단하는 기능 추가)
    ptr[n]로 된것은 해당 ptr[n]을  함수의 인자에 넣으면 된다는 것입니다.

--- prompt.c
+++ prompt.c
@@ -17,9 +17,9 @@ void init(void)
 {
 	char prompt[1000];
 	char *word;
-	char *ptr[4];
+	char *ptr[5];
 	int i=0;
-		for(int j=0;j<4;j++)
+		for(int j=0;j<5;j++)
 			ptr[j]=(char*)malloc(sizeof(char)*50);
 		printf("input prompt:");
 		scanf("%[^\n]",prompt);
@@ -29,7 +29,68 @@ void init(void)
 			i++;
 			word=strtok(NULL," ");
 		}
-		for(int j=0;j<4;j++)
-			printf("%s\n",ptr[j]);
 
+		if(strcmp("myls",ptr[0])==0){
+			if(strcmp("-l",ptr[1])==0)
+				printf("i am myls -l\n");
+			else if(strcmp("-i",ptr[1])==0)
+				printf("i'm myls -i\n");
+			else 
+				printf("i'm myls \n");
+		}
+		else if(strcmp("mycat",ptr[0])==0){
+			if(strcmp(">",ptr[1])==0){
+				printf("i'm mycat > ptr[2]\n");
+				//ptr[2]에 파일명 집어 넣고 함수의 인자로 ptr[2]집어 넣으면 됩니다
+			}
+			else
+				printf("i'm mycat ptr[1]\n");
+		}
+		else if(strcmp("myshowfile",ptr[0])==0){
+			printf("i'm myshowfile ptr[1] ptr[2] ptr[3]\n");
+		}
+		else if(strcmp("mypwd",ptr[0])==0){
+			printf("i'm mypwd\n");
+		}
+		else if(strcmp("mycd",ptr[0])==0){
+			printf("i'm mycd ptr[1]\n");
+		}
+		else if(strcmp("mycp",ptr[0])==0){
+			printf("i'm mycp ptr[1] ptr[2]\n");
+		}
+		else if(strcmp("mycpto",ptr[0])==0){
+			printf("i'm mycpto ptr[1] ptr[2]\n");
+		}
+		else if(strcmp("mycpfrom",ptr[0])==0){
+			printf("i'm mycpfrom ptr[1] ptr[2]\n");
+		}
+		else if(strcmp("mymkdir",ptr[0])==0){
+			printf("i'm mymkdir ptr[1]\n");
+		}
+		else if(strcmp("myrmdir",ptr[0])==0){
+			printf("i'm myrmdir ptr[1]\n");
+		}
+		else if(strcmp("mymv",ptr[0])==0){
+			printf("i'm mymv ptr[1] ptr[2]\n");
+		}
+		else if(strcmp("mytouch",ptr[0])==0){
+			printf("i'm mytouch ptr[1]\n");
+		}
+		else if(strcmp("myshowinode",ptr[0])==0){
+			printf("i'm myshowinode ptr[1]\n");
+		}
+		else if(strcmp("myshowblock",ptr[0])==0){
+			printf("i'm myshowblock ptr[1]\n");
+		}
+		else if(strcmp("mystate",ptr[0])==0){
+			printf("i'm mystate\n");
+		}
+		else if(strcmp("mytree",ptr[0])==0){
+			printf("i'm mytree\n");
+		}
+		else if(strcmp("command",ptr[0])==0){
+			printf("i'm command ptr[1]\n");
+		}
+		else
+			printf("wrong command\n");
 }
